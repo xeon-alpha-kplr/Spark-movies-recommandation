fc269738b737190dcce747de8c49bfb889d5b871
xeon-alpha-kplr <124895383+xeon-alpha-kplr@users.noreply.github.com>
2023-07-07 14:43:36 +0000

    martin

--- Spark-movie-recommendation-main/app/server.py
+++ Spark-movie-recommendation-main/app/server.py
@@ -1,28 +1,34 @@
-import time
-import sys
-import cherrypy
-import os
-from cheroot.wsgi import Server as WSGIServer
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
+import time, sys, cherrypy, os
+
+from cheroot.wsgi import Server as WSGIServer, PathInfoDispatcher
+
 from app import create_app
 
-# Importez les autres bibliothèques nécessaires
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+
+# load spark context
+
+from app import create_app
+import cherrypy
 
 conf = SparkConf().setAppName("movie_recommendation-server")
-
+# IMPORTANT: pass aditional Python modules to each worker
+# Init spark context and load libraries
 sc = SparkContext(conf=conf, pyFiles=['engine.py', 'app.py'])
 
-"Spark-movie-recommendation-main/app/ml-latest/ratings.csv" = sys.argv[1] if len(sys.argv) > 1 else ""
-"Spark-movie-recommendation-main/app/ml-latest/ratings.csv" = sys.argv[2] if len(sys.argv) > 2 else ""
+# Get the paths to the movies and ratings dataset
+movies_set_path = sys.argv[1] if len(sys.argv) > 1 else ""
+ratings_set_path = sys.argv[2] if len(sys.argv) > 2 else ""
+    
 
-app = create_app(sc, "Spark-movie-recommendation-main/app/ml-latest/ratings.csv", "Spark-movie-recommendation-main/app/ml-latest/ratings.csv")
+app = create_app(sc, movies_set_path, ratings_set_path)
 
 cherrypy.tree.graft(app.wsgi_app, '/')
-cherrypy.config.update({
-    'server.socket_host': '0.0.0.0',
-    'server.socket_port': 5000,
-    'engine.autoreload.on': False
-})
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 5432,
+                        'engine.autoreload.on': False
+                        })
 
-cherrypy.engine.start()
+if __name__ == '__main__':
+    cherrypy.engine.start()
